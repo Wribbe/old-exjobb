be27ae0caec60e0ebda4ffbb6a5f5623f9987540
Stefan Eng <eng_steff@hotmail.com>
2018-12-13 20:54:46 +0100

    Add survey db symlink to repo.

--- config.py
+++ config.py
@@ -5,4 +5,4 @@ import os
 PATH_DIR_ROOT=os.path.dirname(os.path.abspath(__file__))
 PATH_DIR_TEX=os.path.join(PATH_DIR_ROOT, "tex")
 PATH_DIR_FIGURES =os.path.join(PATH_DIR_ROOT, "out", "figures")
-PATH_DATABASE="/home/seng/backup/backup/shotgun_web/2018-12-09/databases/shotgun_web_surveys.sqlite"
+PATH_DATABASE="db_surveys.sqlite"
